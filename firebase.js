8a27423d7e263ae94e27db9bde48eceac9450afe
Igor Semiz <Igor.semiz89@gmail.com>
2019-01-28 12:02:30 +0100

    uppdated

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'; 
+
+
+      const config = {
+        apiKey: "AIzaSyCIlU0adPdklL4cvZ_F1vQ7oBK-97L30yM",
+        authDomain: "rental-car-1ae21.firebaseapp.com",
+        databaseURL: "https://rental-car-1ae21.firebaseio.com",
+        projectId: "rental-car-1ae21",
+        storageBucket: "rental-car-1ae21.appspot.com",
+        messagingSenderId: "564944240462"
+      };
+      firebase.initializeApp(config);
+   
+
+firebase.initializeApp(config);
+export default firebase;
